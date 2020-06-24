744c0a717f37daa39b343787f2c845d2c6d7de5f
Michal Piekarczyk <namoopsoo>
2020-06-23 23:42:08 -0400

    moar

--- fresh/preproc/v1.py
+++ fresh/preproc/v1.py
@@ -6,14 +6,13 @@ def preprocess(X, y, neighborhoods):
     # Initially assuming labeled=True
     labeled = True
     genders = [0, 1, 2]
-    user_types = ['Subscriber', 'Customer']
-    time_of_day = [0, 1, 2, 3, 4]
 
     enc = OneHotEncoder(handle_unknown='error', 
-                        categories=[neighborhoods, genders, time_of_day])
+                        categories=[neighborhoods, genders])
+
     enc.fit(X)
     X_transformed = enc.transform(X)
-    
+
     le = LabelEncoder()
     le.fit(y)  # previously on neighborhoods
     
--- fresh/preproc/v2.py
+++ fresh/preproc/v2.py
@@ -1,3 +1,4 @@
+import numpy as np
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 
 # dataset v2: start neighborhod and gender
@@ -5,8 +6,47 @@ from sklearn.preprocessing import OneHotEncoder, LabelEncoder
 #   and customer|subscriber
 #   and birth year one hot..
 
-def preprocess(X, y, neighborhoods):
-    #
+def preprocess(X, y, neighborhoods, labeled, proc_dict=None):
+    num_rows = X.shape[0]
 
-    'usertype'  # 'Customer', 'Subscriber'
+    if not labeled:
+        X_transformed = xform(X, prod_dict)
+    else:
+
+        #
+        'usertype'  # 'Customer', 'Subscriber'
+        genders = [0, 1, 2]
+        user_types = ['Subscriber', 'Customer']
+        time_of_day = [0, 1, 2, 3, 4]
+        features = ['start_neighborhood', 'gender', 'time_of_day', 'usertype', 'weekday', ]
+
+        enc = OneHotEncoder(handle_unknown='error', 
+                            categories=[neighborhoods, genders, time_of_day])
+
+        # first 3 ....
+        enc.fit(X[:, :3])
+        X_transformed = enc.transform(X[:, :3])
+
+        usertype_le = LabelEncoder()
+        usertype_le.fit(X[:, 3])
+        
+        le = LabelEncoder()
+        le.fit(y)  # previously on neighborhoods
+
+        proc_dict = {'enc': enc, 'usertype_le': usertype_le, 'le': le}
+        
+        y_enc = le.transform(y)    
+        
+        return X_transformed, enc, le, y_enc
+
+def xform(X, prod_dict):
+    X_transformed = np.hstack((
+        proc_dict['enc'].transform(X[:, :3]),
+        np.resize(
+            proc_dict['usertype_le'].transform(X[:, 3]),
+            (num_rows, 1)
+            ),
+        X[:, 4:5]
+        ))
+    return X_transformed
 
