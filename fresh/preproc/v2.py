1a59b8da52e17f221572bc4d79c80eda7eb7d2cd
Michal Piekarczyk <namoopsoo>
2020-06-27 18:49:40 -0400

    libsvm write

--- fresh/preproc/v2.py
+++ fresh/preproc/v2.py
@@ -1,7 +1,7 @@
 import numpy as np
 from tqdm import tqdm
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
-import sklearn.datasets import dump_svmlight_file
+from sklearn.datasets import dump_svmlight_file
 import fresh.utils as fu
 
 # dataset v2: start neighborhod and gender
@@ -20,7 +20,7 @@ If given (workdir, dataset_name), write the result there.
     num_rows = X.shape[0]
 
     if proc_bundle:
-        X_transformed, y_enc = xform(proc_bundle, X, y, workdir, dataset_name)
+        X_transformed, y_enc = xform(proc_bundle, X, y, workdir, dataset_name, filetype='libsvm')
         return X_transformed, y_enc
     else:
         genders = [0, 1, 2]
@@ -39,39 +39,50 @@ If given (workdir, dataset_name), write the result there.
         le = LabelEncoder()
         le.fit(y)  # previously had used neighborhoods here
         proc_bundle = {'enc': enc, 'usertype_le': usertype_le, 'le': le}
-        outfile = xform(proc_bundle, X, y, workdir, dataset_name)
+        outfile = xform(proc_bundle, X, y, workdir, dataset_name, filetype='libsvm')
         return proc_bundle, outfile
         
 
-def xform(proc_bundle, X, y=None, workdir=None, dataset_name=None):
+def xform(proc_bundle, X, y=None, workdir=None, dataset_name=None, filetype=None):
     '''Apply preprocessing to X, y.  '''
-
-    #
     num_rows = X.shape[0]
     slices = fu.get_slices(list(range(num_rows)), num_slices=10)
-    outfile = f'{workdir}/{dataset_name or "data"}.csv'
+    for a, b in tqdm(slices):
+        slice_size = b - a
+        stacks = [
+            proc_bundle['enc'].transform(X[a:b, :3]).toarray(),
+            np.resize(
+                proc_bundle['usertype_le'].transform(X[a:b, 3]),
+                (slice_size, 1)
+                ),
+            X[a:b, 4:5]]
+        X_transformed = np.hstack(stacks)
+
+        if y is not None:
+            y_enc = proc_bundle['le'].transform(y[a:b])
+        else:
+            y_enc = None
+
+        outfile = f'{workdir}/{dataset_name or "data"}.{filetype}'
+        if filetype == 'csv':
+            to_csv(X_transformed, y_enc, outfile)
+        elif filetype == 'libsvm':
+            to_libsvm(X_transformed, y_enc, outfile)
+    return outfile
+
+
+def to_csv(X, y, outfile):
+    stacks = [X]
+    if y is not None:
+        stacks.insert(0, np.resize(
+            y,
+            (y.shape, 1)))
+    data = np.hstack(stacks)
     with open(outfile, 'ab') as fd:
-        for a, b in tqdm(slices):
-            slice_size = b - a
-            stacks = [
-                proc_bundle['enc'].transform(X[a:b, :3]).toarray(),
-                np.resize(
-                    proc_bundle['usertype_le'].transform(X[a:b, 3]),
-                    (slice_size, 1)
-                    ),
-                X[a:b, 4:5]
-                ]
-            #
-            if y is not None:
-                stacks.insert(0, np.resize(
-                    proc_bundle['le'].transform(y[a:b]),
-                    (slice_size, 1)))
+        np.savetxt(fd, data, delimiter=',', fmt='%u')
 
-            X_transformed = np.hstack(stacks)
-            np.savetxt(fd, X_transformed, delimiter=',', fmt='%u')
 
-        #X_transformed_parts.append(X_transformed)
-    #X_transformed = np.concatenate(X_transformed_parts)
-
-    return outfile #X_transformed, y_enc
+def to_libsvm(X, y, outfile):
+    with open(outfile, 'ab') as fd:
+        dump_svmlight_file(X, y, outfile)
 
