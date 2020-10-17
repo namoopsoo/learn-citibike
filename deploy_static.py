83afc44fee972c07dcec89f98b727fc1fb77de45
Michal Piekarczyk <namoopsoo>
2020-10-17 12:23:29 -0400

    deploy foo

--- deploy_static.py
+++ deploy_static.py
@@ -1,6 +1,8 @@
-
 import os
+import boto3
 import fresh.s3utils as fs3
+import fresh.utils as fu
+
 
 def deploy():
     deployables = ['index.html', 'js/fetch_data.js']
@@ -20,8 +22,41 @@ def deploy():
             content_type = None
         fs3.write_s3_file(deploy_bucket, s3fn, blah, content_type=content_type)
 
-def main():
-    pass
+
+def read_git_hash():
+    with open('fresh/git_hash.txt') as fd: 
+        return fd.read().split()[0]
+
+
+def deploy_lambda():
+    s3loc = s3_lambda_zip_push()
+    client = boto3.client('lambda',
+            region_name='us-east-1'
+            )
+    client.update_function_code(
+        FunctionName=os.getenv('BIKELEARN_LAMBDA'),
+        # ZipFile=b'bytes',
+        S3Bucket=os.getenv('S3_LAMBDA_ARTIFACTS_BUCKET'),
+        S3Key=s3loc,
+        # S3ObjectVersion='string',
+        Publish=True,
+        DryRun=False,
+        # RevisionId=last_revision_id
+    )
+
+
+def s3_lambda_zip_push():
+    deploy_bucket = os.getenv('S3_LAMBDA_ARTIFACTS_BUCKET')
+    assert deploy_bucket
+    local_loc = 'foo.zip'
+    s3fn = f'{os.getenv("BIKELEARN_LAMBDA")}/{read_git_hash()}/{local_loc}'
+    with open(local_loc, 'rb') as fd:
+        blah = fd.read()
+    fs3.write_s3_file(deploy_bucket, s3fn, blah)
+    print(f'deployed to {s3fn}')    
+    return s3fn
+
 
 if __name__ == '__main__':
+    print('deploying')
     deploy()
