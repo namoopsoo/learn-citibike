6aa020043096fd3e379139e45b0e060f59f47735
Michal Piekarczyk <namoopsoo>
2020-10-25 19:29:35 -0400

    add hack

--- fresh/lambda_entry.py
+++ fresh/lambda_entry.py
@@ -90,15 +90,16 @@ def get_start_location(record, bundle):
             dict(df.iloc[0]), ['latlng', 'station_name'])
 
 
+def make_csvdata(record, header):
+    return ','.join([str(record[k]) for k in header])
+
+
 def call_sagemaker(record):
     header = ['starttime',
             'start station name',
             'usertype',
             'birth year',
             'gender']
-
-
-    csvdata = ','.join([str(record[k]) for k in header])
 
     if os.getenv('IN_LAMBDA'):
         client = boto3.client('sagemaker-runtime',
@@ -107,33 +108,47 @@ def call_sagemaker(record):
                 region_name='us-east-1')
         #
         try:
-            endpoint = os.getenv('SAGEMAKER_ENDPOINT')
-            response = client.invoke_endpoint(
-                    EndpointName=endpoint,
-                    # Body=b'bytes'|file,
-                    Body=csvdata,
-                    ContentType='text/csv',
-                    Accept='string',
-                    # CustomAttributes='string'
-                    )
-            what = response['Body'].read()
-            return json.loads(what)
+            return _call_inner(record, header)
         except botocore.exceptions.ClientError as e:
             error = {'error_detail': repr(e),
                      'error': e.__class__.__name__}
-            return error
+            
+            hack_city_starttime = '2020-10-01 18:10:00'
+            print('Grrr that weird bug, changing the starttime from ',
+                    record['starttime'], ' to ', hack_city_starttime)
+            print(error)
+            record['starttime'] = hack_city_starttime
+            return _call_inner(record, header)
 
     else:
         url = LOCAL_URL
         headers = {'Content-Type': 'text/csv'}
         # tripduration,starttime,stoptime,start station id,start station name,start station latitude,start station longitude,end station id,end station name,end station latitude,end station longitude,bikeid,usertype,birth year,gender\n
 
-
+        csvdata = make_csvdata(record, header)
+        print('DEBUG call_sagemaker, csvdata, ', csvdata)
         r = requests.post(url, data=csvdata, headers=headers)
         assert r.status_code/100 == 2, \
                 'got this instead, r.status_code ' + str(r.status_code)
 
         return r.json()
+
+
+def _call_inner(record, header):
+    csvdata = make_csvdata(record, header)
+    print('DEBUG call_sagemaker, csvdata, ', csvdata)
+
+    endpoint = os.getenv('SAGEMAKER_ENDPOINT')
+    response = client.invoke_endpoint(
+            EndpointName=endpoint,
+            # Body=b'bytes'|file,
+            Body=csvdata,
+            ContentType='text/csv',
+            Accept='string',
+            # CustomAttributes='string'
+            )
+    what = response['Body'].read()
+    return json.loads(what)
 
 
 def map_probabilities(bundle, prob_vec, k=5):
